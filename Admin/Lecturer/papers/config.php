9e5ad3082eb5b28bfa91548d73d7d9288b8b175d
umindu97 <41871891+umindu97@users.noreply.github.com>
2019-04-27 11:36:04 +0530

    updated paper(admin/papers/index.php)

---
+++ Admin/Lecturer/papers/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'webuni';
+$databaseUsername = 'root';
+$databasePassword = 'root';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+?>
